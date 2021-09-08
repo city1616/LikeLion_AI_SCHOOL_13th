c9d5985b753105dffd675e089272c933eaa8a3fc
MunSeungWoo <57985497+city1616@users.noreply.github.com>
2021-09-08 17:16:23 +0900

    네이버 영화 정보 수정

--- 02. 웹과 Github 기본/05_web_data/08_movie_info.py
+++ 02. 웹과 Github 기본/05_web_data/08_movie_info.py
@@ -1,7 +1,8 @@
-# 2021_09_08_6
+# 2021_09_08_6 7
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import pandas as pd
 
 # url 정보
 # tag, id, class 정보
@@ -33,3 +34,26 @@ for idx, one in enumerate(score_data) :
     score.append(one.find("span", class_ = "num").text)
 print(score)
 print()
+
+# 예매율 정보 가져오기
+ticket_data = soup.find_all("dl", class_ = "info_exp")
+ticketing = []
+for idx, one in enumerate(ticket_data) :
+    print(idx + 1, one.find("span", class_ = "num").text)
+    ticketing.append(one.find("span", class_ = "num").text)
+print(ticketing)
+
+# 참여 명수 가져오기
+person_data = soup.find_all("span", class_ = "num2")
+# print(person_data[1].find("em").text)
+cnt_person = []
+for idx, one in enumerate(person_data) :
+    print(idx + 1, one.find("em").text)
+    cnt_person.append(one.find("em").text)
+print(cnt_person)
+
+# 타이틀, 평점, 평점 참여명수, 예매율
+# 파일 만들기
+dict_dat = {"영화제목" : movie_title, "평점" : score, "평점 참여명수" : cnt_person}
+movie_df = pd.DataFrame(dict_dat)
+print(movie_df)
