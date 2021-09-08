d8cbba46ae5d1f186a376977c1d308141e7fbea4
MunSeungWoo <57985497+city1616@users.noreply.github.com>
2021-09-08 09:09:49 +0900

    js 수정

--- 02. 웹과 Github 기본/02_web_html/demo_3.js
+++ 02. 웹과 Github 기본/02_web_html/demo_3.js
@@ -172,7 +172,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(10,217,249,'+ _this.active+')';
             ctx.fill();
         };
     }
