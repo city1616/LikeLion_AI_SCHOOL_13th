7ce49ce5a21012d82db5e3326fa080bc8da06ae6
MunSeungWoo <57985497+city1616@users.noreply.github.com>
2021-09-17 21:25:50 +0900

    project 파일 수정

--- 02. 웹과 Github 기본/05_web_data/개인 프로젝트/project.py
+++ 02. 웹과 Github 기본/05_web_data/개인 프로젝트/project.py
@@ -4,7 +4,20 @@
 # 병원약국 - 세부 조건별 찾기 xpath :  //*[@id="header"]/div[2]/ul/li[1]/div/div/ul/li[1]/ul/li[2]/a
 
 from  selenium import webdriver
+import time
 
 url = "https://www.hira.or.kr/main.do" 				# 건강보험심사평가원 url
-driver = webdriver.Chrome("./DATA/chromedriver")
+driver = webdriver.Chrome("../DATA/chromedriver")
 driver.get(url)										# url 접속
+
+# implicitly wait
+# 10초로 설정하면 페이지가 로딩되는데 10초까지 기다린다.
+# 만약 페이지 로딩이 2초에 완료되었다면 더 기다리지 않고 다음 코드 실행한다.
+driver.implicitly_wait(10)
+# time.sleep(3)
+
+
+
+
+first_button = driver.find_element_by_xpath('//*[@id="header"]/div[2]/ul/li[1]/div/div/ul/li[1]/ul/li[2]/a')
+first_button.click()
