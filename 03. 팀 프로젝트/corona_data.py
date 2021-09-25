19428249509bcbf91a6c078eb4d6bbb6de09304b
MunSeungWoo <57985497+city1616@users.noreply.github.com>
2021-09-25 16:33:13 +0900

    coroona data 수집 파일 수정

--- 03. 팀 프로젝트/corona_data.py
+++ 03. 팀 프로젝트/corona_data.py
@@ -7,14 +7,15 @@ from urllib.request import Request, urlopen
 from urllib.parse import urlencode, quote_plus
 
 url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson'
-servicekey = 'GCyZSspoE7iPOuN%2B4UKO10fOhJyXmQhnhGwke3KcD8v2qaWYLQkoGLNn7vTwbctZkR%2F3uEECJAykpO4UJxUwTQ%3D%3D'
+servicekey = 'GCyZSspoE7iPOuN+4UKO10fOhJyXmQhnhGwke3KcD8v2qaWYLQkoGLNn7vTwbctZkR/3uEECJAykpO4UJxUwTQ=='
 queryParams = '?' + urlencode({ quote_plus('ServiceKey') : servicekey, quote_plus('pageNo') : '1', quote_plus('numOfRows') : '10', quote_plus('startCreateDt') : '20200310', quote_plus('endCreateDt') : '20200315' })
 
 request = Request(url + queryParams)
 request.get_method = lambda: 'GET'
 response_body = urlopen(request).read()
 print(response_body)
-print()
+
+print(type(response_body))
 
 '''
 import requests
