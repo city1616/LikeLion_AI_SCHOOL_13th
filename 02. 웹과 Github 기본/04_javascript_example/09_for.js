f94887a053e869df33e3958b56cc637aebd60528
MunSeungWoo <57985497+city1616@users.noreply.github.com>
2021-11-17 20:00:37 +0900

    파일 정리

--- 02. 웹과 Github 기본/04_javascript_example/09_for.js
+++
@@ -1,7 +0,0 @@
-// 2021_09_07_05
-var cars = ['BMW', 'Volvo', 'Saab', 'Ford', 'Fiat', 'Audi'];
-var text = "";
-var i;
-for(i = 0; i < cars.length; i++) {
-    alert(cars[i]);
-}
--- 02. 웹과 Github 기본/04_javascript_example/10_ex.js
+++
@@ -1,4 +0,0 @@
-// 2021_09_07_05
-function change_p() {
-    document.getElementById("demo").innerHTML = "Hello";
-}
